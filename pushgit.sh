64ade4d0fb4f70576cec2c7a861becd2d75d0e57
Guillermo Valle <guillefix@gmail.com>
2017-01-06 19:55:01 -0500

    updated cosmos

--- pushgit.sh
+++ pushgit.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+HOME=/deploy
+LOGNAME=deploy
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+LANG=en_US.UTF-8
+SHELL=/bin/sh
 
 git add -A
 git commit -m "\"updated cosmos\""
