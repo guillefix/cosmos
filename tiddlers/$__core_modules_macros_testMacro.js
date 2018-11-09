223123a21f019351e800b003cd47b39f7d28e255
Guillerm Valle <guillefix@gmail.com>
2018-11-09 12:56:37 -0500

    static

---
+++ tiddlers/$__core_modules_macros_testMacro.js
@@ -0,0 +1,6 @@
+created: 20181109175206855
+modified: 20181109175206855
+title: /home/deploy/cosmos.git/tiddlers/$__core_modules_macros_testMacro.js
+tmap.id: a377dc8b-2607-4256-9742-71dd2b75800e
+type: text/vnd.tiddlywiki
+
