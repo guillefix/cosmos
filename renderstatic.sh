b3d020a4fd1ee9a3110689b492d8c8ffc29a3dcb
Guillerm Valle <guillefix@gmail.com>
2018-11-09 17:49:57 -0500

    new script

---
+++ renderstatic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mv /home/deploy/cosmos.git/tiddlers/\$__core_modules_macros_3Dmol.js.tid /home/deploy/cosmos.git
+
+tiddlywiki /home/deploy/cosmos.git --render "[!is[system]] +[!prefix[$:/]]" "[encodeuricomponent[]addprefix[static/]addsuffix[.html]]" text/plain $:/core/templates/static.tiddler.html
+
+mv /home/deploy/cosmos.git/\$__core_modules_macros_3Dmol.js.tid /home/deploy/cosmos.git/tiddlers
+
+tiddlywiki /home/deploy/cosmos.git --rendertiddler $:/core/templates/static.template.html static.html text/plain
+
+tiddlywiki /home/deploy/cosmos.git --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
