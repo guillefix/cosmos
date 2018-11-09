9ba8c504514ced7fced4b5de9df8704bfeb1f73e
Guillerm Valle <guillefix@gmail.com>
2018-11-09 17:55:11 -0500

    "updated cosmos"

--- renderstatic.sh
+++ renderstatic.sh
@@ -6,6 +6,8 @@ tiddlywiki /home/deploy/cosmos.git --render "[!is[system]] +[!prefix[$:/]]" "[en
 
 mv /home/deploy/cosmos.git/\$__core_modules_macros_3Dmol.js.tid /home/deploy/cosmos.git/tiddlers
 
+rename  's/%20/\ /g' /home/deploy/cosmos.git/output/static/*
+
 tiddlywiki /home/deploy/cosmos.git --rendertiddler $:/core/templates/static.template.html static.html text/plain
 
 tiddlywiki /home/deploy/cosmos.git --rendertiddler $:/core/templates/static.template.css static/static.css text/plain
